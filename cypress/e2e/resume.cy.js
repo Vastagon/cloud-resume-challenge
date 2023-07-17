80acabb7575e94c17e0b87c503a7aaff5c1e56b0
Vastagon <jacob.yancey29@yahoo.com>
2023-07-16 23:46:31 -0500

    Updating CI/CD for tests

---
+++ cypress/e2e/resume.cy.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+describe("My awesome website", () => {
+    beforeEach(() => {
+        cy.visit("https://vastagon.azureedge.net/")
+    })
+
+    it("Has my name as the title", () => {
+        cy.get("h2").should("contain.text", "Jacob")
+    })
+
+    it("Visit Count exists", () => {
+        cy.get("[id=visit-count]").should("have.length", 1)
+    })
+
+    it("Correctly shows the visit count of the website", () => {
+        const temp = document.getElementById("visit-count")
+        cy.get("[id=visit-count]").invoke("text").then((text) => {
+            expect(text.length).to.be.at.least(12)
+        })
+    })
+})
