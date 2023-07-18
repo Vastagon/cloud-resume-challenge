9d089ba8800099db2861b3f1cdc85b8f4c17dceb
Vastagon <jacob.yancey29@yahoo.com>
2023-07-18 01:58:14 -0500

    Got ARM Template working for Azure Functions. Last thing to do is find a way to add \n to my python code on each line to properly configure teh function app

--- InputTrigger/__init__.py
+++ InputTrigger/__init__.py
@@ -10,22 +10,4 @@ def main(req: func.HttpRequest, inputDocument: func.DocumentList):
         totalCount['count'] = updatedCount
         return totalCount
     else:
-        return func.Document.from_dict({"id": "count", "count": 0})
-    
-
-
-temp = """
-import azure.functions as func \n
-\n
-\n
-def main(req: func.HttpRequest, inputDocument: func.DocumentList):\n
-    if inputDocument:\n
-        totalCount = inputDocument[0]\n
-        updatedCount = totalCount['count']\n
-        updatedCount = updatedCount + 1\n
-\n
-        totalCount['count'] = updatedCount\n
-        return totalCount\n
-    else:\n
-        return func.Document.from_dict({"id": "count", "count": 0})\n
-"""
+        return func.Document.from_dict({ "id": "count", "count": 0 })
--- resume/ARM Templates/CosmosDB build.ps1
+++ resume/ARM Templates/CosmosDB build.ps1
@@ -13,9 +13,16 @@ az deployment group create --resource-group $rg --template-file ".\CosmosDB\temp
 # Create Function App with in binding to increase the counter stored in NoSQL CosmosDB
 $templateUri = ".\Azure Functions\template.json"
 $rg = "ResumeWebsiteAPI";
-
+$FunctionFilePath = "..\..\InputTrigger\__init__.py"
 New-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateFile $templateUri
 ### CREATE THE FUNCTION APP YOURSELF USING THE CODE IN InputTrigger. Make sure CORS only works with the website's URI
+$FunctionFilePath = "D:\Code\cloud-resume-challenge\InputTrigger\__init__.py"
+
+# Get the content for the function
+$FunctionFilePath = "..\..\InputTrigger\__init__.py"
+$FunctionFileContent = Get-Content -Path $FunctionFilePath
+az deployment group create --subscription "Pay as you go Production" --resource-group $rg --template-file $templateUri --parameters ((@{ __init__file = $FunctionFileContent }))
+
 
 
 # Fix the context and locations when I can actually put things where I want them after throttle is over
@@ -32,3 +39,73 @@ New-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateFile ".\Monitoring
 New-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateFile ".\Monitoring\API Latency\template.json"
 
 
+
+
+"files": {
+    "__init__.py": "import azure.functions as func \n\n\ndef main(req: func.HttpRequest, inputDocument: func.DocumentList):\n    if inputDocument:\n        totalCount = inputDocument[0]\n        updatedCount = totalCount['count']\n        updatedCount = updatedCount + 1\n\n        totalCount['count'] = updatedCount\n        return totalCount\n    else:\n        return func.Document.from_dict({'id': 'count', 'count': 0})\n"
+}
+
+
+$templateUri = ".\Azure Functions\template.json"
+$rg = "ResumeWebsiteAPI";
+$accountName = "resumecosmosvastagon"
+$FunctionFilePath = "..\..\InputTrigger\__init__.py"
+
+# Get the content for the function
+$FunctionFileContent = Get-Content -Path $FunctionFilePath
+# Get connection string
+$cosmosDBConnectionString = (Get-AzCosmosDBAccountKey -ResourceGroupName $rg -Name $accountName -Type "ConnectionStrings")."Primary SQL Connection String"
+# Deploy the Function app
+az deployment group create --subscription "Pay as you go Production" --resource-group $rg --template-file $templateUri --parameters initpy=$FunctionFileContent cosmosDBConnectionString=$cosmosDBConnectionString
+
+
+
+
+# ## Updates FunctionAppSetting for the connection string
+# $cosmosDBConnectionString = (Get-AzCosmosDBAccountKey -ResourceGroupName $rg -Name $accountName -Type "ConnectionStrings")."Primary SQL Connection String"
+# Update-AzFunctionAppSetting -Name "vastagonresumecountapi" -ResourceGroupName $rg -AppSetting @{"CosmosDbConnectionString" = $cosmosDBConnectionString }
+
+
+
+
+## MIGHT BE ABLE TO CREATE A FOREACH THAT ADDS \n TO EACH LINE
+
+
+
+
+import azure.functions as func \n\n\ndef main(req: func.HttpRequest, inputDocument: func.DocumentList):\n    if inputDocument:\n        totalCount = inputDocument[0]\n        updatedCount = totalCount['count']\n        updatedCount = updatedCount + 1\n\n        totalCount['count'] = updatedCount\n        return totalCount\n    else:\n        return func.Document.from_dict({ 'id': 'count', 'count': 0 })\n
+
+
+
+
+import azure.functions as func
+
+    
+def main(req: func.HttpRequest, inputDocument: func.DocumentList):
+if inputDocument:
+totalCount = inputDocument[0]
+updatedCount = totalCount['count']
+updatedCount = updatedCount + 1
+
+totalCount['count'] = updatedCount
+return totalCount
+else:
+return func.Document.from_dict({ "id": "count", "count": 0 })
+    
+
+
+# temp = """
+# import azure.functions as func \n
+# \n
+# \n
+# def main(req: func.HttpRequest, inputDocument: func.DocumentList):\n
+#     if inputDocument:\n
+#         totalCount = inputDocument[0]\n
+#         updatedCount = totalCount['count']\n
+#         updatedCount = updatedCount + 1\n
+# \n
+#         totalCount['count'] = updatedCount\n
+#         return totalCount\n
+#     else:\n
+#         return func.Document.from_dict({"id": "count", "count": 0})\n
+# """
